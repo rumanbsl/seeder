e2e9bdadd146b5875a39c4c8155a4a12f34186df
Majedul Hoque <rumanbsl@gmail.com>
2019-11-29 18:33:50 +0200

    moderate config

--- build-configs/webpack.config.js
+++ build-configs/webpack.config.js
@@ -4,19 +4,20 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 
 const mode = process.env.NODE_ENV || "production";
+const PROJECT_ROOT = resolve(__dirname, "..");
 
 module.exports = {
   mode,
-  entry: resolve(__dirname, "..", "src", "index.js"),
+  entry: `${PROJECT_ROOT}/src/index.js`,
   devServer: {
     contentBase: [
-      resolve(__dirname, "..", "dist"),
-      resolve(__dirname, "..", "public"),
+      `${PROJECT_ROOT}/dist`,
+      `${PROJECT_ROOT}/public`,
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({  // Also generate a test.html
-      template: resolve(__dirname, "..", "src", "index.html")
+      template: `${PROJECT_ROOT}/src/index.html`
     }),
     new CopyPlugin([
       { from: 'public', to: '.' },
